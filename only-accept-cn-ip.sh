5209b34c6b33f58a97250d42fb00892c2d8c6a97
comwrg <xcomwrg@gmail.com>
2018-12-05 14:37:56 +0800

    add accept local address

--- only-accept-cn-ip.sh
+++ only-accept-cn-ip.sh
@@ -7,6 +7,13 @@ ipset -X cn
 # Create the ipset list
 ipset -N cn hash:net
 
+# add local ip
+for i in $(ip a | grep 'inet ' | awk '{print $2}')
+do
+    ipset -A cn $i
+done
+
+# add cn ip
 for i in $(wget -qO- http://www.ipdeny.com/ipblocks/data/countries/cn.zone)
 do
     ipset -A cn $i
